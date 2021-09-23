f36172091c29bc6e7c5616ef1d072c264c8d1e49
VISHAL GUPTA <vishal.kr.gupta@gmail.com>
2021-09-23 06:35:44 +0530

    added collecttion-item listing

--- src/Components/collection-preview/collection-preview.component.jsx
+++ src/Components/collection-preview/collection-preview.component.jsx
@@ -9,7 +9,7 @@ const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
-                .map(({id, ...otherItemProps }) => (
+                .map(({ id, ...otherItemProps }) => (
                     <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
