61f3fec305b8ae3ab18a66bd294f1a40ea8dbb5c
VISHAL GUPTA <vishal.kr.gupta@gmail.com>
2021-09-24 07:25:58 +0530

    added sign-in-sign-out and sign-in functionality

--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -12,16 +12,16 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit=event=>{
+    handleSubmit = event => {
         event.preventDefault();
 
-        this.state({email:'',password:''})
+        this.state({ email: '', password: '' })
     }
 
-    handleChange=event=>{
+    handleChange = event => {
 
-        const {value, name}=event.target;
-        this.setState({[name]:value})
+        const { value, name } = event.target;
+        this.setState({ [name]: value })
     }
 
     render() {
@@ -30,17 +30,18 @@ class SignIn extends React.Component {
                 <h2>
                     I alredy have an account
                 </h2>
+
                 <span>
                     Sign in with your email and password
                 </span>
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" value={this.state.email} 
-                    onChange={this.handleChange}
-                    required />
+                    <input name="email" type="email" value={this.state.email}
+                        onChange={this.handleChange}
+                        required />
                     <label>Email</label>
-                    <input name="password" type="password" value={this.state.password} 
-                    onChange={this.handleChange}
-                    required />
+                    <input name="password" type="password" value={this.state.password}
+                        onChange={this.handleChange}
+                        required />
                     <label>Password</label>
 
                     <input type="submit" value="Submit Form" ></input>
@@ -50,3 +51,5 @@ class SignIn extends React.Component {
 
     }
 }
+
+export default SignIn;
