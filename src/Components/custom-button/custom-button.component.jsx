4f2ec26a846996ce442a7d6e1e8df48b784fc0c2
VISHAL GUPTA <vishal.kr.gupta@gmail.com>
2021-09-25 07:39:18 +0530

    Added custom form-input and custom button

---
+++ src/Components/custom-button/custom-button.component.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import './custom-button.styles.scss';
+
+const CustomButton=({children,...otherButtonProps})=>(
+<button className='custom-button' {...otherButtonProps}>
+    {children}
+</button>
+);
+
+export default CustomButton;
--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -35,16 +37,16 @@ class SignIn extends React.Component {
                     Sign in with your email and password
                 </span>
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" value={this.state.email}
-                        onChange={this.handleChange}
+                    <FormInput name="email" type="email" value={this.state.email}
+                        handleChange={this.handleChange}
+                        label='email'
                         required />
-                    <label>Email</label>
-                    <input name="password" type="password" value={this.state.password}
-                        onChange={this.handleChange}
+                    <FormInput name="password" type="password" value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password'
                         required />
-                    <label>Password</label>
 
-                    <input type="submit" value="Submit Form" ></input>
+                    <CustomButton type='submit'>Sign in</CustomButton>
                 </form>
             </div>
         )
