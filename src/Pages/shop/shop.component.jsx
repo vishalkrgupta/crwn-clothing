1a05423cae8733942f373c22da0c7ba951191485
VISHAL GUPTA <vishal.kr.gupta@gmail.com>
2022-07-17 17:52:43 +0530

    shop component

--- src/Pages/shop/shop.component.jsx
+++ src/Pages/shop/shop.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from "../../Components/collection-overview/collection-overview.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => 
+{
+    console.log(match);
+    return (
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact={`${match.path}`} component={CollectionsOverview} />
     </div>
-);
+)};
 
 export default ShopPage;
